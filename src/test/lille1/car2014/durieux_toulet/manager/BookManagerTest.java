e16cf08edc1158b990b3d9948a218eef52dc7d6e
Cyrille Toulet <iwa@cloverleaf-ii>
2014-05-08 17:16:04 +0200

    Improve manager tests

--- src/test/lille1/car2014/durieux_toulet/manager/BookManagerTest.java
+++ src/test/lille1/car2014/durieux_toulet/manager/BookManagerTest.java
@@ -1,6 +1,7 @@
 package test.lille1.car2014.durieux_toulet.manager;
 
 import lille1.car2014.durieux_toulet.manager.BookManager;
+import lille1.car2014.durieux_toulet.DAO.BookFinder;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import org.junit.Test;
@@ -13,16 +14,26 @@ public class BookManagerTest {
         // Instanciate book manager
         BookManager bm = new BookManager();
 
+        // Book data
+        String title = "Game_of_thrones";
+        String author = "Hodor";
+        int year = 2013;
+        double price = 20.50;
+
         // Set book data
-        bm.setBookTitle("Game_of_thrones");
-        bm.setBookAuthor("Hodor");
-        bm.setBookYear(2013);
-        bm.setBookPrice(20.50);
+        bm.setBookTitle(title);
+        bm.setBookAuthor(author);
+        bm.setBookYear(year);
+        bm.setBookPrice(price);
 
         // Create book
         bm.createBook();
 
-        //assertEquals(?, ?);
+        // Do tests
+        assertEquals(title, BookFinder.INSTANCE.getBook(title).getTitle())
+        assertEquals(author, BookFinder.INSTANCE.getBook(title).getAuthor())
+        assertEquals(year, BookFinder.INSTANCE.getBook(title).getYear())
+        assertEquals(price, BookFinder.INSTANCE.getBook(title).getPrice())
     }
 
 }
--- src/test/lille1/car2014/durieux_toulet/manager/UserManagerTest.java
+++ src/test/lille1/car2014/durieux_toulet/manager/UserManagerTest.java
@@ -1,6 +1,7 @@
 package test.lille1.car2014.durieux_toulet.manager;
 
 import lille1.car2014.durieux_toulet.manager.UserManager;
+import lille1.car2014.durieux_toulet.DAO.UserFinder;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import org.junit.Test;
@@ -13,16 +14,26 @@ public class UserManagerTest {
         // Instanciate user manager
         UserManager um = new UserManager();
 
+        // user data
+        String firstname = "Jack";
+        String lastname = "Sparrow";
+        String username = "jack";
+        String password = "1234";
+
         // Set user description
-        um.setUsername("jack");
-        um.setPassword("1234");
-        um.setFname("Jack");
-        um.setLname("Sparrow");
+        um.setUsername(username);
+        um.setPassword(password);
+        um.setFname(firstname);
+        um.setLname(lastname);
 
         // Create user
         um.createUser();
 
-        //assertEquals(?, ?);
+        // Do tests
+        assertEquals(firstname, UserFinder.INSTANCE.getUserFromUsername(username).getFirstname())
+        assertEquals(lastname, UserFinder.INSTANCE.getUserFromUsername(username).getLastname())
+        assertEquals(username, UserFinder.INSTANCE.getUserFromUsername(username).getUsername())
+        assertEquals(password, UserFinder.INSTANCE.getUserFromUsername(username).getPassword())
     }
 
 }
