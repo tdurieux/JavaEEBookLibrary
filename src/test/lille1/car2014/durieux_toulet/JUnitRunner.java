5e2d195429716d3cf059994c5937fe123c163c53
Cyrille Toulet <iwa@cloverleaf-ii>
2014-05-08 16:06:35 +0200

    Creation of JUnit runner

---
+++ src/test/lille1/car2014/durieux_toulet/JUnitRunner.java
@@ -0,0 +1,33 @@
+package test.lille1.car2014.durieux_toulet;
+
+//import test.lille1.car2014.durieux_toulet.manager.BookManagerTest;
+//import test.lille1.car2014.durieux_toulet.manager.UserManagerTest;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+/**
+ * @author Toulet Cyrille
+ */
+public class JUnitRunner {
+
+    /**
+     * Main
+     */
+    public static void main(final String[] args) {
+        // Test BookManager
+        /*Result result = JUnitCore.runClasses(BookManagerTest.class);
+
+        for (final Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }*/
+
+        /* Test UserManager */
+        /*result = JUnitCore.runClasses(UserManagerTest.class);
+
+        for (final Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }*/
+    }
+}
